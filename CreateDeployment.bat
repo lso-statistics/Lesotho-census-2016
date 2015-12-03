797abd4ec1e8fdca6e7e7f36952a66f214a589be
Josh Handley <joshua.e.handley@census.gov>
2015-12-03 15:18:36 +0200

    Initial commit of version 11 of Census applications

---
+++ CreateDeployment.bat
@@ -0,0 +1,48 @@
+setlocal
+
+
+REM Find CSEntry.exe (path differs on 32 and 64 bit Windows)
+
+SET CSEntry="%ProgramFiles(x86)%\CSPro 6.1\CSEntry.exe"
+if exist %CSEntry% goto :gotcspro
+SET CSEntry="%ProgramFiles%\CSPro 6.1\CSEntry.exe"
+if exist %CSEntry% goto :gotcspro
+echo "Can't find CSEntry version 6.1. Is it installed?"
+goto :eof
+:gotcspro
+
+
+REM Create deployment directory
+rmdir /q /s Deployment
+mkdir Deployment
+cd Deployment
+mkdir LesothoCensus2016
+cd LesothoCensus2016
+mkdir HouseholdDataEntry
+mkdir Menu
+mkdir AssistSupReview
+cd..\..
+
+REM Create .pen files
+%CSEntry% /pen .\Menu\LesothoCensusMenu.ent
+%CSEntry% /pen .\HouseholdDataEntry\HouseholdDataEntry.ent
+%CSEntry% /pen .\AssistSupReview\AssistSupReview.ent
+
+REM copy lookup files
+copy /y .\Menu\staff.dat .\Deployment\LesothoCensus2016\Menu
+copy /y .\Menu\AssSup.dat .\Deployment\LesothoCensus2016\Menu
+
+REM Copy applications to deployment
+move /y .\Menu\LesothoCensusMenu.pen .\Deployment\LesothoCensus2016\Menu
+move /y .\HouseholdDataEntry\HouseholdDataEntry.pen .\Deployment\LesothoCensus2016\HouseholdDataEntry
+move /y .\AssistSupReview\AssistSupReview.pen .\Deployment\LesothoCensus2016\AssistSupReview
+
+REM Copy .pff for Menu
+copy /y .\Menu\*.pff .\Deployment\LesothoCensus2016\Menu
+
+REM Copy .pnc files
+copy /y Client.pnc .\Deployment\LesothoCensus2016
+copy /y Server.pnc .\Deployment\LesothoCensus2016
+copy /y SupervisorInitialSync.pnc .\Deployment\LesothoCensus2016
+copy /y SyncWithServer.pnc .\Deployment\LesothoCensus2016
+copy /y SyncWithSupervisor.pnc .\Deployment\LesothoCensus2016
