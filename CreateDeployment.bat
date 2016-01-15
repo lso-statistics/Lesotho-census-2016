dc13730fa05bb20ff8c9463557071a2b7fdb9c33
Prince <l.seeisok@gmail.com>
2016-01-15 19:29:30 +0200

    - modified the batch file to create the staff.dat look up file - modified the sync files - got rid of the AssSup dictionary as it was not used

--- CreateDeployment.bat
+++ CreateDeployment.bat
@@ -3,8 +3,10 @@ setlocal
 REM Find CSEntry.exe (path differs on 32 and 64 bit Windows)
 
 SET CSEntry="%ProgramFiles(x86)%\CSPro 6.2\CSEntry.exe"
+SET Excel2CSPro="%ProgramFiles(x86)%\CSPro 6.2\Excel2CSPro.exe"
 if exist %CSEntry% goto :gotcspro
 SET CSEntry="%ProgramFiles%\CSPro 6.2\CSEntry.exe"
+SET Excel2CSPro="%ProgramFiles%\CSPro 6.2\Excel2CSPro.exe"
 if exist %CSEntry% goto :gotcspro
 echo "Can't find CSEntry version 6.2. Is it installed?"
 goto :eof
@@ -38,10 +40,10 @@ REM Create .pen files
 %CSEntry% /pen .\Menu\LesothoCensusMenu.ent
 %CSEntry% /pen .\HouseholdDataEntry\HouseholdDataEntry.ent
 %CSEntry% /pen .\AssistSupReview\AssistSupReview.ent
+%Excel2CSPro% /run /excel=".\Menu\staff.xlsx" /dcf=".\Menu\Staff.dcf" /output=".\Menu\staff.dat" /utf8="1" /worksheet="1" /rowStart="2" /columns="1+"
 
 REM copy lookup files
 copy /y .\Menu\staff.dat .\Deployment\LesothoCensus2016\Application\Menu
-copy /y .\Menu\AssSup.dat .\Deployment\LesothoCensus2016\Application\Menu
 
 REM Copy applications to deployment
 move /y .\Menu\LesothoCensusMenu.pen .\Deployment\LesothoCensus2016\Application\Menu
